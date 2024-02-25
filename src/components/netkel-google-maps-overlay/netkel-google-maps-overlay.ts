8f03338d63be2681e5429475fd35388f60563852
Germán <german1983@gmail.com>
2024-02-25 18:11:39 -0500

    ADd Google maps overlay initial setup

---
+++ src/components/netkel-google-maps-overlay/netkel-google-maps-overlay.ts
@@ -0,0 +1,44 @@
+import { LitElement, html } from "lit";
+import { customElement, property } from "lit/decorators";
+import { styles } from './netkel-google-maps-overlay.styles';
+
+@customElement('netkel-google-maps-overlay')
+export class NetKelGoogleMapsOverlay extends LitElement {
+  static styles = styles;
+
+  @property({ type: String })
+  apiKey = '';
+
+  @property({ type: String })
+  overlayImageSourceUrl = '';
+
+  @property({ type: String })
+  pinCoordinates = '';
+
+  @property({ type: String })
+  pinName = '';
+
+  static getMetaConfig() {
+      return import('./netkel-google-maps-overlay.config')
+        .then(({ config }) => {
+          return config;
+      });
+  }
+
+  render() {
+      return html`<div id="map">
+        <span>This is the apiKey configured:<strong>@this.apiKey</strong></span>
+        <ul>
+          <li>Overlay Image Source URL: <strong>@this.overlayImageSourceUrl</strong></li>
+          <li>Pin Coordinates: <strong>@this.pinCoordinates</strong></li>
+          <li>Pin Name: <strong>@this.pinName</strong></li>
+        </ul>
+      </div>`;
+  }
+
+  firstUpdated() {
+      // Load the Google Maps API
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${this.apiKey}&callback=initMap`;
+  }
+}
